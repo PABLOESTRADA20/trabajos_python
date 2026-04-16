b4d2917a0147d4479da7deef31c7e8063e071ef7
PABLOESTRADA20 <pablo.maximiliano.cocio.estrada@gmail.com>
2026-04-16 13:12:28 -0400

    Agregar scripts para contar vocales y consonantes en un nombre, y calcular el promedio de notas

---
+++ for/consonante.py
@@ -0,0 +1,11 @@
+nombre = input("¿Cuál es tu nombre? ")
+letrs = 0
+letras = 0
+num_consonantes = 0
+for i in nombre:
+    letras = letras + 1
+    if i in " aeiouAEIOU":
+        letrs = letrs + 1
+    elif i in " bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ": 
+            num_consonantes = num_consonantes + 1
+print(f"tu nombre tiene {letrs} vocales , tiene  {num_consonantes} consonantes y {letras} letras en total")
